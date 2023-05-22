9bf2d22e1d2fa30f64dcf97c17db62cd1d85b767
Jagurian <antoinesoto19977@gmail.com>
2023-05-22 16:34:40 +0200

    Initial commit

---
+++ printRandomNumber.js
@@ -0,0 +1,11 @@
+function printHello() { // definition de la fonction  printHello
+    let x = Math.floor((Math.random() * 10) + 1); 
+    console.log(x);
+  }
+  
+  printHello(); // appel de la fonction printHello
+
+
+  
+
+  
